f2e5664fd8361c804bd397c2c3595291cdc3c437
Highbeekhay <118852889+Highbeekhay@users.noreply.github.com>
2023-04-05 12:53:14 +0100

    pin input

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:d_chart/d_chart.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -11,7 +12,81 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(15, 15, 15, 25),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(
+            width: 2,
+            color: Colors.grey.shade800,
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Accepted orders',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.shade800,
+                  ),
+                  child: Row(
+                    children: const [
+                      Text('Week'),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            AspectRatio(
+              aspectRatio: 20 / 10,
+              child: DChartBarCustom(
+                showDomainLine: true,
+                showMeasureLine: true,
+                showDomainLabel: true,
+                showMeasureLabel: true,
+                spaceBetweenItem: 8,
+                listData: [
+                  DChartBarDataCustom(
+                      value: 450, label: 'Sun', color: Colors.cyan.shade50),
+                  DChartBarDataCustom(
+                      value: 300, label: 'Mon', color: Colors.cyan.shade50),
+                  DChartBarDataCustom(
+                      value: 350, label: 'Tue', color: Colors.cyan.shade50),
+                  DChartBarDataCustom(
+                      value: 400, label: 'Wed', color: Colors.cyan.shade50),
+                  DChartBarDataCustom(
+                      value: 350, label: 'Thu', color: Colors.cyan.shade50),
+                  DChartBarDataCustom(
+                      value: 450, label: 'Fri', color: Colors.cyan.shade50),
+                  DChartBarDataCustom(
+                      value: 200, label: 'Sat', color: Colors.cyan.shade50),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/enter_pin_page.dart
+++ lib/enter_pin_page.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:pin_plus_keyboard/package/controllers/pin_input_controller.dart';
+import 'package:pin_plus_keyboard/pin_plus_keyboard.dart';
 
 class EnterPinPage extends StatefulWidget {
-  const EnterPinPage({super.key});
+  const EnterPinPage({Key? key}) : super(key: key);
 
   @override
   State<EnterPinPage> createState() => _EnterPinPageState();
 }
 
 class _EnterPinPageState extends State<EnterPinPage> {
+  PinInputController pinInputController = PinInputController(length: 6);
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
+      appBar: AppBar(),
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Enter your PIN",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w300,
+                    fontSize: size.width * 0.05),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            PinPlusKeyBoardPackage(
+              buttonBorderColor: Colors.transparent,
+              keyboardButtonShape: KeyboardButtonShape.defaultShape,
+              keyoardBtnBorderRadius: BorderRadius.circular(5),
+              keyboardMaxWidth: 50,
+              inputHasBorder: false,
+              inputFillColor: Colors.transparent,
+              buttonFillColor: Colors.grey.shade800,
+              btnTextColor: Colors.white,
+              spacing: size.height * 0.06,
+              pinInputController: pinInputController,
+              inputTextColor: Colors.white,
+              onSubmit: () {
+                /// ignore: avoid_print
+                print("Text is : " + pinInputController.text);
+              },
+              keyboardFontFamily: '',
+            ),
+          ],
+        ),
       ),
     );
   }
